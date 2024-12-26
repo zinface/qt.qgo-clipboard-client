b66a0f6f4f65dffe97c084e1d7fbd9da70b266c4
zinface <zinface@163.com>
2024-12-26 19:54:22 +0800

    feat: 优化窗口居中逻辑并添加版本信息
    - 使用 spark-framework-features 库中的 ScreenUtil 类实现窗口居中
    - 在应用启动时显示版本信息
    - 移除自定义 moveCenter 函数

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,20 @@
 #include <QMainWindow>
 
 #include "config.h"
-
-void moveCenter(MainWindow &wm) {
-    QDesktopWidget *desktop = QApplication::desktop();
-    wm.move((desktop->width() - wm.width()) /2,(desktop->height() - wm.height()) /2);
-}
+#include <screenutil.h>
+#include <version.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("qgo-clipboard-client");
-//    a.setApplicationDisplayName("");
-   
+    a.setApplicationDisplayName("云剪贴板 - v" PROJECT_VERSION);
+    a.setApplicationVersion(PROJECT_VERSION);
+
     MainWindow wm;
-    moveCenter(wm);
+    wm.show();
+
+    ScreenUtil::moveCenterForCursor(wm);
 
     qDebug() << "[ClipBoard Server]: " << REMOTE_HOST;
     return a.exec();
