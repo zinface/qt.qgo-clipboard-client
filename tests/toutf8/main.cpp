4f2402eef951b358373f121ddb2a64503ad3f936
zinface <zinface@163.com>
2024-12-26 23:45:42 +0800

    test(toutf8): 添加日志注释用于调试和测试
    - 在 main 函数中添加了多个 DEBUG 日志注释
    - 日志注释展示了不同编码转换和剪贴板图片数据获取的过程

--- tests/toutf8/main.cpp
+++ tests/toutf8/main.cpp
@@ -36,3 +36,14 @@ int main(int argc, char *argv[]) {
         << gbk->toUnicode(QByteArray::fromBase64(s.toUtf8()));
 
 }
+
+
+// main(12): DEBUG  QString -> Base64
+// main(14): DEBUG  获取剪贴板图片数据 6I635Y+W5Ymq6LS05p2/5Zu+54mH5pWw5o2u
+// main(17): DEBUG  QString -> GBK -> Base64
+// main(18): DEBUG  获取剪贴板图片数据 ��ȡ������ͼƬ���� u/HIobz0zPmw5c28xqzK/b7d
+// main(25): DEBUG  Base64 -> QString
+// main(27): DEBUG  6I635Y+W5Ymq6LS05p2/5Zu+54mH5pWw5o2u 获取剪贴板图片数据
+// main(31): DEBUG  Base64 -> GBK -> QString
+// main(33): DEBUG  u/HIobz0zPmw5c28xqzK/b7d ��ȡ������ͼƬ���� 获取剪贴板图片数据
+
