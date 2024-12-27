eaf8f27fea3c47e241e9c716d507e085792c3416
zinface <zinface@163.com>
2024-12-27 09:44:46 +0800

    test(toutf8): 补充GBK到UTF-8再到Base64的测试
    - 新增GBK到UTF-8再到Base64的测试用例
    - 优化代码格式，调整缩进和空行

--- tests/toutf8/main.cpp
+++ tests/toutf8/main.cpp
@@ -3,20 +3,20 @@
 #include "qd.h"
 #include "../src/utils/Base64Image.cpp"
 int main(int argc, char *argv[]) {
-    
+
     QCoreApplication a(argc, argv);
-    
+
     QTextCodec *utf8 = QTextCodec::codecForName("UTF-8");
     QTextCodec *gbk = QTextCodec::codecForName("GBK");
 
     qd << "QString -> Base64";
     QString s = "获取剪贴板图片数据";
-    qd << s 
+    qd << s
         << Base64Text::fromText(s);
 
     qd << "QString -> GBK -> Base64";
-    qd << s 
-        << gbk->fromUnicode(s.toUtf8()) 
+    qd << s
+        << gbk->fromUnicode(s.toUtf8())
         << gbk->fromUnicode(s.toUtf8()).toBase64();
     // qd << s.toLocal8Bit().toBase64();
     // qd << codec->fromUnicode(s.toLocal8Bit()).toBase64();
@@ -24,17 +24,21 @@ int main(int argc, char *argv[]) {
     // 6I635Y+W5Ymq6LS05p2/5Zu+54mH5pWw5o2u
     qd << "Base64 -> QString";
     s = "6I635Y+W5Ymq6LS05p2/5Zu+54mH5pWw5o2u";
-    qd << s 
+    qd << s
         << Base64Text::fromBase64(s);
 
     // u/HIobz0zPmw5c28xqzK/b7d
     qd << "Base64 -> GBK -> QString";
     s = "u/HIobz0zPmw5c28xqzK/b7d";
-    qd << s 
-        << Base64Text::fromBase64(s) 
+    qd << s
+        << Base64Text::fromBase64(s)
         // << gbk->toUnicode(Base64ByteArray::fromBase64(s))
         << gbk->toUnicode(QByteArray::fromBase64(s.toUtf8()));
 
+    qd << "GBK -> UTF-8 -> Base64";
+    s = "获取剪贴板图片数据";
+    qd << s.toUtf8().toBase64();       // toUtf8: Windows Utf8，Linux Utf8
+    qd << s.toLocal8Bit().toBase64();  // toLocal8Bit: Windows GBK，Linux Utf8
 }
 
 
